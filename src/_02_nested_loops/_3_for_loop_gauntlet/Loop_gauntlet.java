60095753c49cbe84dd18a336eb7f2eebdc70be66
root <root@21796db289a1>
2020-06-17 22:28:58 +0000

    gauntlet

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Loop_gauntlet.java
@@ -0,0 +1,51 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class Loop_gauntlet {
+	public static void main(String[] args) {
+//	for (int i = 0; i < 101; i++) {
+//		System.out.println(i);
+//	}
+//	for (int i = 100; i > -1; i--) {
+//		System.out.println(i);
+//	}
+//	for (int i = 2; i < 101; i++) {
+//		if(i%2==0) {System.out.println(i);}
+//	}
+//	for (int i = 1; i < 101; i++) {
+//		if(i%2==1) {System.out.println(i);}
+//	}
+//		for (int i = 1; i < 501; i++) {
+//			if (i % 2 == 1) {
+//				System.out.println(i + " is odd");
+//			} else if (i % 2 == 0) {
+//				System.out.println(i + " is even");
+//			}
+//		}
+//		int num = 7;
+//		int ber =7;
+//		for (int i = 0; i < 111; i++) {
+//			System.out.println(num);
+//			num+=ber;
+//		}
+//		int age=2005;
+//		for (int i = 0; i < 15; i++) {
+//			System.out.println("in "+age+" i was " +i+" years old");
+//			age++;
+//		}
+//		System.out.println("this year im going to be 15 years old");
+//		int Num = 0;
+//		int Ber = 0;
+//		for (int i = 0; i < 3; i++) {
+//			for (int j = 0; j < 3; j++) {
+//				System.out.println(Num + " " + Ber);
+//				Ber++;
+//			}
+//			Num++;
+//			Ber = 0;
+//		}
+		for (int i = 1; i < 9; i++) {
+			System.out.println(i+""+i + "");
+		}
+
+	}
+}
