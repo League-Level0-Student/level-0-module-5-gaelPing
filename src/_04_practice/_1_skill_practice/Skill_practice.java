fa849f641abcbb7d8141ef858b93db03be8fede4
root <root@f78eefa46dfb>
2020-06-25 22:28:50 +0000

    gam

--- src/_04_practice/_1_skill_practice/Skill_practice.java
+++ src/_04_practice/_1_skill_practice/Skill_practice.java
@@ -1,7 +1,17 @@
 package _04_practice._1_skill_practice;
 
+import javax.swing.JOptionPane;
+
 public class Skill_practice {
 public static void main(String[] args) {
+JOptionPane.showInputDialog("how many dimes do you have.");
+JOptionPane.showInputDialog("how tall are you in inches.");
+
+
+
+
+
+
 
 }
 
