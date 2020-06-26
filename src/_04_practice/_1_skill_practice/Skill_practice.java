ff59b20c235407554177b6c9a73a99e511394204
root <root@d05a633a7758>
2020-06-26 22:26:53 +0000

    skill

--- src/_04_practice/_1_skill_practice/Skill_practice.java
+++ src/_04_practice/_1_skill_practice/Skill_practice.java
@@ -1,22 +1,64 @@
 package _04_practice._1_skill_practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class Skill_practice {
-public static void main(String[] args) {
-JOptionPane.showInputDialog("how many dimes do you have.");
-JOptionPane.showInputDialog("how tall are you in inches.");
-
-
-
-
-
-
+	public static void main(String[] args) {
+//String dimes=JOptionPane.showInputDialog("how many dimes do you have.");
+//int ten=Integer.parseInt(dimes);
+//ten*=10;
+//System.out.println(ten);
+//String inches=JOptionPane.showInputDialog("how tall are you in inches.");
+//int inch=Integer.parseInt(inches);
+//if (inch<=36) {
+//	System.out.println("eat your wheaties"); 
+//}
+//else {
+//	System.out.println("your surprisingly tall");
+//}
+//
+//for (int i = 1; i < 31;i++) {
+//	if (i% 3==0) {
+//		System.out.println(i);
+//	}
+//}
+//
+//Random robert  = new Random(20-0);
+//
+//System.out.print(robert);
+//
+//String city=JOptionPane.showInputDialog("where do you live.");
+//
+//if (city.equals("San Diego")) {
+//	System.out.println("you live in America's finest city.");
+//	
+//}
+//else {
+//	System.out.println("you should move to San Diego.");
+//}
+//		
+//int wheel=4;
+//	String car=JOptionPane.showInputDialog("how many cars does yoiur familey have ");	
+//		if (car.equals("0")) {
+//			System.out.println("i bet you use public transport.");
+//			
+//		}
+//		else if (car.equals("1")) {
+//			System.out.println("your car has "+ wheel+" wheels.");
+//		}
+//		
+//		else if (car.equals("2")) {
+//			wheel*=2;
+//			System.out.println("your cars have "+ wheel+" wheels combined.");
+//		}
+//
+//	String school=JOptionPane.showInputDialog("what is the name of your school.");	
+//		System.out.println(school+" is an amazing school.");
+	}
 
 }
-
-}
-	
 
 //		SkillPractice skills = new SkillPractice();
 //		           skills.skill1();
